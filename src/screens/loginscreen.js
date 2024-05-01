22b44d31fe5be4604740cd71c7c5bd734cfddfa6
Chiranthan Y <mechiranthan.y@gmail.com>
2024-05-01 15:29:56 +0530

    inital commit

---
+++ src/screens/loginscreen.js
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export class loginscreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>loginscreen</Text>
+      </View>
+    );
+  }
+}
+
+export default loginscreen;
---
+++ src/screens/registerscree.js
@@ -0,0 +1,14 @@
+import {Text, View} from 'react-native';
+import React, {Component} from 'react';
+
+export class registerscree extends Component {
+  render() {
+    return (
+      <View>
+        <Text>registerscree</Text>
+      </View>
+    );
+  }
+}
+
+export default registerscree;
